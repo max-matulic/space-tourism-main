8740cb5fd14e2cb8e06406e0b4d48759c1ba082a
max.matulic <max.matulic@ibmix.hr>
2025-10-02 10:00:15 +0200

    create destination page

--- src/app/pages/destination/destination.component.ts
+++ src/app/pages/destination/destination.component.ts
@@ -1,9 +1,33 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { destinations } from '../../../data/data.json';
+import { Destination } from './models/destination.interface';
 @Component({
   selector: 'app-destination',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './destination.component.html',
   styleUrl: './destination.component.scss',
 })
-export class DestinationComponent {}
+export class DestinationComponent {
+  destinationList: Destination[] = destinations;
+  filteredDestination: Destination[] = [
+    {
+      name: 'Moon',
+      images: {
+        png: './assets/destination/image-moon.png',
+        webp: './assets/destination/image-moon.webp',
+      },
+      description:
+        'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+      distance: '384,400 km',
+      travel: '3 days',
+    },
+  ];
+
+  getDestination(destination: string) {
+    this.filteredDestination = this.destinationList.filter(
+      (dest) => dest.name === destination
+    );
+    return this.filteredDestination;
+  }
+}
---
+++ src/app/pages/destination/models/destination.interface.ts
@@ -0,0 +1,10 @@
+export interface Destination {
+  description: string;
+  distance: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  name: string;
+  travel: string;
+}
